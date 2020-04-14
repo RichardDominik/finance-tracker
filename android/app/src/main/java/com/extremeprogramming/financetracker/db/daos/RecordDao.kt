15eedb7e4707fae3f10562935e5c24ba4b397fb9
Denis Capkovic <capkovicdenis@gmail.com>
2020-04-14 16:12:58 +0200

    Remove wrong relation from db

--- android/app/src/main/java/com/extremeprogramming/financetracker/db/daos/RecordDao.kt
+++ android/app/src/main/java/com/extremeprogramming/financetracker/db/daos/RecordDao.kt
@@ -7,22 +7,6 @@ import java.util.*
 
 @Dao
 interface RecordDao {
-    @Transaction
-    @Query("SELECT * FROM Record")
-    fun getAll() : List<RecordWithCategory>
-
-    @Transaction
-    @Query("SELECT * FROM Record WHERE dstrftime('%m', date) = :month")
-    fun getAllByMonth(month:Int) : List<RecordWithCategory>
-
-    @Transaction
-    @Query("SELECT * FROM Record ORDER BY date DESC LIMIT 10")
-    fun getLastTenRecords() : List<RecordWithCategory>
-
-    @Transaction
-    @Query("SELECT * FROM Record WHERE date = :date")
-    fun getAllByDate(date: Date) : List<RecordWithCategory>
-
     @Query("DELETE FROM Record")
     fun deleteAll()
 
