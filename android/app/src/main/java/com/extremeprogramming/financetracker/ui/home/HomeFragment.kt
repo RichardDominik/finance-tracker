6d5d9ea71a4ef7fb37fb73eda439d7e629f5f350
Denis Capkovic <capkovicdenis@gmail.com>
2020-04-14 16:18:12 +0200

    Add skeleton with dummy data to homescreen

--- android/app/src/main/java/com/extremeprogramming/financetracker/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/extremeprogramming/financetracker/ui/home/HomeFragment.kt
@@ -8,12 +8,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.extremeprogramming.financetracker.R
+import com.extremeprogramming.financetracker.db.entities.Category
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import kotlin.random.Random
 
 class HomeFragment : Fragment() {
-
     private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var pieChart: PieChart
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,10 +26,35 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
+
+        pieChart = root.findViewById(R.id.pie_char_month_categories)
+
+        val balanceTextView = root.findViewById<TextView>(R.id.text_view_balance_value)
+        homeViewModel.balance.observe(this, Observer {
+            balanceTextView.text = it.toString()
         })
+
+        val monthBalanceTextView = root.findViewById<TextView>(R.id.text_view_month_balance_value)
+        homeViewModel.monthBalance.observe(this, Observer {
+            monthBalanceTextView.text = it.toString()
+        })
+
+        homeViewModel.monthSpending.observe(this, Observer {
+            updatePieChart(it)
+        })
+
         return root
+    }
+
+    private fun updatePieChart(categories: List<Category>) {
+        val values = ArrayList<PieEntry>()
+        for (category in categories) {
+            values.add(PieEntry(category.budget!!.toFloat(), category.name))
+        }
+
+        val dataSet = PieDataSet(values, "")
+        val data = PieData(dataSet)
+        pieChart.data = data
+        pieChart.invalidate()
     }
 }
--- android/app/src/main/java/com/extremeprogramming/financetracker/ui/home/HomeViewModel.kt
+++ android/app/src/main/java/com/extremeprogramming/financetracker/ui/home/HomeViewModel.kt
@@ -3,11 +3,56 @@ package com.extremeprogramming.financetracker.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.extremeprogramming.financetracker.db.entities.Category
+import com.extremeprogramming.financetracker.db.entities.Record
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 class HomeViewModel : ViewModel() {
+    private val _balance = MutableLiveData<Double>().apply { value = 123.32 }
+    val balance: LiveData<Double> = _balance
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _monthBalance = MutableLiveData<Double>().apply { value = -123.32 }
+    val monthBalance: LiveData<Double> = _monthBalance
+
+    private val _monthSpending = MutableLiveData<List<Category>>()
+    val monthSpending: LiveData<List<Category>> = _monthSpending
+
+    private val _recentTransactions = MutableLiveData<List<Record>>()
+    val recentTransactions: LiveData<List<Record>> = _recentTransactions
+
+    init {
+        loadDummyCategories()
+        loadDummyRecords()
     }
-    val text: LiveData<String> = _text
+
+    private fun loadDummyCategories() {
+        viewModelScope.launch {
+            val categories = ArrayList<Category>()
+            repeat((5..12).random()) {
+                delay(100)
+                categories.add(Category(it, it.toString(), budget = Random.nextDouble() * 182))
+            }
+
+            _monthSpending.postValue(categories)
+        }
+    }
+
+    private fun loadDummyRecords() {
+        viewModelScope.launch {
+            val records = ArrayList<Record>()
+            repeat(5) {
+                delay(100)
+                records.add(Record(it, it.toString(), Random.nextDouble() * 123, null, 1, Calendar.getInstance().time))
+            }
+
+            _recentTransactions.postValue(records)
+        }
+    }
 }
