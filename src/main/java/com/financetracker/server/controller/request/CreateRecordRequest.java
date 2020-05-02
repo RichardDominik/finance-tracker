ab76e78392e7b60aac537b282269b9951c96f9a6
janaoravcova <janora78@gmail.com>
2020-05-02 11:03:58 +0200

    record controller modified(ResponseEntity added, request class modified)

--- src/main/java/com/financetracker/server/controller/request/CreateRecordRequest.java
+++ src/main/java/com/financetracker/server/controller/request/CreateRecordRequest.java
@@ -1,13 +1,13 @@
 package com.financetracker.server.controller.request;
 
+import com.financetracker.server.data.entity.Category;
 import com.financetracker.server.data.entity.Record;
 
 import java.io.Serializable;
 
 public class CreateRecordRequest implements Serializable {
 
-    private String categoryId;
-    private String userId;
+    private Category category;
     private Record record;
 
     public Record getRecord() {
@@ -18,19 +18,11 @@ public class CreateRecordRequest implements Serializable {
         this.record = record;
     }
 
-    public String getUserId() {
-        return userId;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
+    public void setCategoryId(Category category) {
+        this.category = category;
     }
 }
