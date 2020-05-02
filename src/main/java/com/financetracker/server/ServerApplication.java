ba0b7fd63c2c0042859d893393b7409e38158c65
janaoravcova <janora78@gmail.com>
2020-05-02 10:56:46 +0200

    authentication + athorization filters

--- src/main/java/com/financetracker/server/ServerApplication.java
+++ src/main/java/com/financetracker/server/ServerApplication.java
@@ -2,6 +2,8 @@ package com.financetracker.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,5 +14,10 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
