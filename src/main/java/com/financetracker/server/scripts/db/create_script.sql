7df06c9a85d58eae5199133de8e2f6aa16f7deb4
Richard <riso.dominik@gmail.com>
2020-06-05 13:55:05 +0200

    on delete cascade

--- src/main/java/com/financetracker/server/scripts/db/create_script.sql
+++ src/main/java/com/financetracker/server/scripts/db/create_script.sql
@@ -8,7 +8,7 @@ create table users(
 drop table if exists categories cascade;
 create table categories(
       id serial primary key,
-      user_id integer references users(id) not null,
+      user_id integer references users(id) on delete cascade not null,
       name varchar not null,
       description varchar,
       budget numeric check (budget >= 0),
@@ -21,8 +21,8 @@ create type record_type as enum ('credit', 'debit');
 drop table if exists records;
 create table records(
       id serial primary key,
-      user_id integer references users(id) not null,
-      category_id integer references categories(id) not null,
+      user_id integer references users(id) on delete cascade not null,
+      category_id integer references categories(id) on delete cascade not null,
       type record_type not null,
       description varchar,
       amount numeric not null
