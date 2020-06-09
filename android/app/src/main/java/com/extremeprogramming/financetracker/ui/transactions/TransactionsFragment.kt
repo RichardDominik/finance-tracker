eafb19c224828b1b1f5b567ad1a7a432ef30a010
Denis Capkovic <capkovicdenis@gmail.com>
2020-06-09 13:46:34 +0200

    Fix after merge

--- android/app/src/main/java/com/extremeprogramming/financetracker/ui/transactions/TransactionsFragment.kt
+++ android/app/src/main/java/com/extremeprogramming/financetracker/ui/transactions/TransactionsFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.extremeprogramming.financetracker.R
 
 class TransactionsFragment : Fragment() {
